70baee1270d60f4c27cc5a7b35b7804f91d24b29
황규희 <gyuhee3021@gmail.com>
2026-05-17 01:12:01 +0900

    [추가] TabContorl 추가 [추가] 타이틀 표시를 위한 label 추가

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -6,6 +6,9 @@
         ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private TabControl tcMain;
+        private TabPage tpDataManager;
+        private TabPage tpTrainingTest;
 
         /// <summary>
         ///  Clean up any resources being used.
@@ -28,12 +31,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            tcMain = new TabControl();
+            tpDataManager = new TabPage();
+            tpTrainingTest = new TabPage();
+            lblTitle = new Label();
+            tcMain.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tcMain
+            // 
+            tcMain.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tcMain.Controls.Add(tpDataManager);
+            tcMain.Controls.Add(tpTrainingTest);
+            tcMain.Location = new Point(0, 40);
+            tcMain.Name = "tcMain";
+            tcMain.SelectedIndex = 0;
+            tcMain.Size = new Size(800, 580);
+            tcMain.TabIndex = 0;
+            // 
+            // tpDataManager
+            // 
+            tpDataManager.Location = new Point(4, 24);
+            tpDataManager.Name = "tpDataManager";
+            tpDataManager.Padding = new Padding(3);
+            tpDataManager.Size = new Size(792, 552);
+            tpDataManager.TabIndex = 0;
+            tpDataManager.Text = "데이터 관리";
+            tpDataManager.UseVisualStyleBackColor = true;
+            // 
+            // tpTrainingTest
+            // 
+            tpTrainingTest.Location = new Point(4, 24);
+            tpTrainingTest.Name = "tpTrainingTest";
+            tpTrainingTest.Padding = new Padding(3);
+            tpTrainingTest.Size = new Size(792, 552);
+            tpTrainingTest.TabIndex = 1;
+            tpTrainingTest.Text = "학습/테스트";
+            tpTrainingTest.UseVisualStyleBackColor = true;
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.BackColor = Color.Transparent;
+            lblTitle.Font = new Font("Lucida Bright", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTitle.ForeColor = Color.FromArgb(255, 114, 16);
+            lblTitle.Location = new Point(0, 0);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(200, 31);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "Data Manager";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(800, 620);
+            Controls.Add(lblTitle);
+            Controls.Add(tcMain);
+            Name = "Form1";
             Text = "Form1";
+            tcMain.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblTitle;
     }
 }
